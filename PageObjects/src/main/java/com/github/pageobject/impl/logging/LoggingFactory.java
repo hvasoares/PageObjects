2af04b3707a2d6bca6d19d3916fec26c5dc43f00
hugo soares <hugo.soares@unp.br>
2013-09-25 19:03:34 -0300

    Logging with log4j.

---
+++ PageObjects/src/main/java/com/github/pageobject/impl/logging/LoggingFactory.java
@@ -0,0 +1,30 @@
+package com.github.pageobject.impl.logging;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import com.github.pageobject.PageObject;
+import com.github.pageobject.impl.ProxyStatePageObjectAdapter;
+import com.github.pageobject.impl.browser.Browser;
+
+public abstract class LoggingFactory {
+	private static Logger log;
+
+	public static ProxyStatePageObjectAdapter createStatePageObjectLogging(){
+		return new StatePageObjectLog(getLog());
+	}
+	
+	public static Browser createBrowserLogging(Browser inner){
+		return new BrowserLog(inner, log);
+	}
+
+	private static Logger getLog() {
+		if(log!=null)
+			return log;
+		log = Logger.getLogger(PageObject.class);
+		BasicConfigurator.configure();
+		log.setLevel(Level.ALL);
+		return log;
+	}
+}
